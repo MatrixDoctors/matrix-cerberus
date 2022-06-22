1c991c16b763d8b19462a81814000690ce526bd8
kuries <bineshmunukurthi@gmail.com>
2022-06-22 22:44:39 +0530

    Add error page and create routes without NavBar Header.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,38 @@
 import './App.css';
 import React from 'react';
-import {Routes, Route, BrowserRouter} from "react-router-dom";
+import {Routes, Route, BrowserRouter, Outlet} from "react-router-dom";
 import AccountSettings from './Pages/AccountSettings';
 import NavBar from './Components/NavBar';
 import RoomSettings from './Pages/RoomSettings';
 import ServerSettings from './Pages/ServerSettings';
 import Login from './Pages/Login';
 import LoginSuccess from './Pages/LoginSuccess';
+import ErrorPage from './Pages/ErrorPage';
 
+function LayoutsWithNavbar() {
+  return (
+    <>
+      <NavBar />
+
+      {/* This Outlet is the place in which react-router will render your components that you need with the navbar */}
+      <Outlet />
+    </>
+  );
+}
 
 export default function App() {
   return (
     <BrowserRouter>
-      <NavBar />
       <Routes>
-        <Route path="/" element={<AccountSettings />} />
-        <Route path="/room-settings" element={<RoomSettings />} />
-        <Route path="/server-settings" element={<ServerSettings />} />
+        <Route path='/' element={<LayoutsWithNavbar/>}>
+          <Route path="/" element={<AccountSettings />} />
+          <Route path="/room-settings" element={<RoomSettings />} />
+          <Route path="/server-settings" element={<ServerSettings />} />
+          <Route path="/login-success" element={<LoginSuccess/>} />
+        </Route>
+
         <Route path="/login" element={<Login />} />
-        <Route path="/login-success" element={<LoginSuccess/>} />
+        <Route path="*" element={<ErrorPage/>} />
       </Routes>
     </BrowserRouter>
   )
