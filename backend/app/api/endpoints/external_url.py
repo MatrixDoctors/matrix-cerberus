b77100a49df0a554c84f1bde55c37b6a8592739e
kuries <bineshmunukurthi@gmail.com>
2022-06-22 21:13:24 +0530

    Add `get_room_invite` and `replace_existing_url` methods to Extrnal Url class and their respetive routes.

--- backend/app/api/endpoints/external_url.py
+++ backend/app/api/endpoints/external_url.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends, Request
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, RedirectResponse
 
 from app.api.deps import fastapi_sessions
 from app.api.models import ExternalUrlInfo
@@ -21,3 +21,31 @@ async def generate_url(
         use_once_only=external_url_info.use_once_only,
     )
     return JSONResponse({"url_code": url_code})
+
+
+@router.get("/{url_code}")
+async def get_invite(
+    request: Request, url_code: str, external_url: ExternalUrl = Depends(ExternalUrl)
+):
+    session_data = fastapi_sessions.get_session(request)
+
+    is_invited = await external_url.get_room_invite(
+        matrix_homeserver=session_data.matrix_homeserver,
+        url_code=url_code,
+        user_id=session_data.matrix_user,
+    )
+    if not is_invited:
+        return RedirectResponse("/login")
+    return RedirectResponse("/")
+
+
+@router.post("/replace-url")
+async def replace_existing_url(
+    request: Request, url_code: str, external_url: ExternalUrl = Depends(ExternalUrl)
+):
+    session_data = fastapi_sessions.get_session(request)
+
+    new_url_code = await external_url.replace_existing_url(
+        matrix_homeserver=session_data.matrix_homeserver, url_code=url_code
+    )
+    return JSONResponse({"url_code": new_url_code})
--- backend/app/matrix/external_url.py
+++ backend/app/matrix/external_url.py
@@ -11,25 +11,60 @@ class ExternalUrl:
 
     def __init__(self):
         self._characters = string.ascii_letters + string.digits
+        self._event_type = "matrix-cerberus.external_url"
 
     def _generate_url_code(self, N: int = 8):
         return "".join(random.choices(self._characters, k=N))
 
-    async def generate_url(self, matrix_homeserver: str, room_id: str, use_once_only: bool):
-        data = await matrix_bot_runner.client.get_account_data(
-            "matrix-cerberus.external_url", matrix_homeserver
-        )
-        external_url_data = data.content
+    async def parse_data_and_save(self, matrix_homeserver, external_url_data):
+        data = {"content": external_url_data}
 
+        await matrix_bot_runner.client.put_account_data(self._event_type, matrix_homeserver, data)
+
+    async def generate_url(self, matrix_homeserver: str, room_id: str, use_once_only: bool):
+        data = await matrix_bot_runner.client.get_account_data(self._event_type, matrix_homeserver)
+        external_url_data = data.content
         url_code = self._generate_url_code()
         while url_code in external_url_data:
             url_code = self._generate_url_code()
 
         external_url_data[url_code] = {"room_id": room_id, "use_once_only": use_once_only}
 
-        data = {"content": external_url_data}
-
-        await matrix_bot_runner.client.put_account_data(
-            "matrix-cerberus.external_url", matrix_homeserver, data
-        )
+        await self.parse_data_and_save(matrix_homeserver, external_url_data)
         return url_code
+
+    async def get_room_invite(self, matrix_homeserver: str, url_code: str, user_id: str):
+        data = await matrix_bot_runner.client.get_account_data(self._event_type, matrix_homeserver)
+        external_url_data = data.content
+
+        if url_code not in external_url_data:
+            return False
+
+        await matrix_bot_runner.client.room_invite(external_url_data[url_code].room_id, user_id)
+
+        if external_url_data[url_code].use_once_only:
+            del external_url_data[url_code]
+
+        await self.parse_data_and_save(matrix_homeserver, external_url_data)
+        return True
+
+    async def replace_existing_url(self, matrix_homeserver: str, url_code: str):
+        data = await matrix_bot_runner.client.get_account_data(self._event_type, matrix_homeserver)
+        external_url_data = data.content
+        room_url_object = external_url_data[url_code]
+
+        # Generate new url and use the old data
+        new_url_code = self._generate_url_code()
+        while new_url_code in external_url_data:
+            new_url_code = self._generate_url_code()
+
+        external_url_data[new_url_code] = {
+            "room_id": room_url_object.room_id,
+            "use_once_only": room_url_object.use_once_only,
+        }
+
+        del external_url_data[url_code]
+
+        await self.parse_data_and_save(matrix_homeserver, external_url_data)
+
+        return new_url_code
