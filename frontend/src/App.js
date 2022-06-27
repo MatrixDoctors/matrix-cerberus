a929dffe8ba3444182b656fed04dc6671fc7cefe
kuries <bineshmunukurthi@gmail.com>
2022-06-27 12:25:14 +0530

    Use context hooks for storing matrix user id after login.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import {Routes, Route, BrowserRouter, Outlet} from "react-router-dom";
 import AccountSettings from './Pages/AccountSettings';
 import NavBar from './Components/NavBar';
@@ -11,6 +11,7 @@ import ErrorPage from './Pages/ErrorPage';
 import ExternalUrl from './Pages/ExternalUrl';
 import CustomRouter from './Components/CustomRouter';
 import history from './HelperFunctions/customHistory';
+import { GlobalContext } from './GlobalContext';
 
 function LayoutsWithNavbar() {
   return (
@@ -24,20 +25,25 @@ function LayoutsWithNavbar() {
 }
 
 export default function App() {
+  const [matrixUserId, setMatrixUserId] = useState('@kuries:matrix.org');
+
   return (
     <CustomRouter history={history}>
+    <GlobalContext.Provider
+          value = {{matrixUserId, setMatrixUserId}}>
       <Routes>
-        <Route path='/' element={<LayoutsWithNavbar/>}>
-          <Route path="/" element={<AccountSettings />} />
-          <Route path="/room-settings" element={<RoomSettings />} />
-          <Route path="/server-settings" element={<ServerSettings />} />
-        </Route>
+          <Route path='/' element={<LayoutsWithNavbar/>}>
+            <Route path="/" element={<AccountSettings />} />
+            <Route path="/room-settings" element={<RoomSettings />} />
+            <Route path="/server-settings" element={<ServerSettings />} />
+          </Route>
 
         <Route path="/i/:url_code" element={<ExternalUrl />} />
         <Route path="/login" element={<Login />} />
         <Route path="/login-success" element={<LoginSuccess/>} />
         <Route path="*" element={<ErrorPage/>} />
       </Routes>
+    </GlobalContext.Provider>
     </CustomRouter>
   )
 };
---
+++ frontend/src/GlobalContext.js
@@ -0,0 +1,6 @@
+import React, { createContext } from 'react'
+
+export const GlobalContext = createContext({
+    matrixUserId: "",
+    setMatrixUserId: () => {}
+});
