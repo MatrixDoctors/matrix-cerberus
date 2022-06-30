b6f85b317a01e75b3246e437bd0174e61b58ea60
kuries <bineshmunukurthi@gmail.com>
2022-06-30 22:31:15 +0530

    Add routes to fetch teams and repos for an org and user.

--- backend/app/api/endpoints/github_routes.py
+++ backend/app/api/endpoints/github_routes.py
@@ -74,13 +74,31 @@ async def authenticate_user(request: Request, body: GithubCode, background_tasks
         raise HTTPException(status_code=500, detail="Internal server error")
 
 
-@router.post("/user")
+@router.get("/user")
 async def get_user(github_api: GithubAPI = Depends(github_api_instance)):
     resp = await github_api.display_user()
     return JSONResponse({"user": resp})
 
 
-@router.post("/orgs")
+@router.get("/orgs")
 async def get_orgs(github_api: GithubAPI = Depends(github_api_instance)):
     resp = await github_api.get_orgs_with_membership()
     return JSONResponse({"orgs": resp})
+
+
+@router.get("/individual-repos")
+async def get_individual_repos(github_api: GithubAPI = Depends(github_api_instance)):
+    resp = await github_api.get_individual_repos()
+    return JSONResponse({"repos": resp})
+
+
+@router.get("/org-repos")
+async def get_org_repos(org: str, github_api: GithubAPI = Depends(github_api_instance)):
+    resp = await github_api.get_repos_in_an_org(org)
+    return JSONResponse({"repos": resp})
+
+
+@router.get("/org-teams")
+async def get_org_teams(org: str, github_api: GithubAPI = Depends(github_api_instance)):
+    resp = await github_api.get_teams_in_an_org(org)
+    return JSONResponse({"teams": resp})
