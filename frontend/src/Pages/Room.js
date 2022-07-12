fcbf0948f4b3990c18215b5ca8d451c700a91a3a
kuries <bineshmunukurthi@gmail.com>
2022-07-12 16:40:22 +0530

    Display modal for add conditions button.
    Update Room.js to display Room page title in medium font for devices with lower viewport size.

--- frontend/src/Pages/Room.js
+++ frontend/src/Pages/Room.js
@@ -1,19 +1,22 @@
 import React, {useState} from "react";
 import { Link, useParams } from "react-router-dom";
+import SelectThirdPartyAccountModal from "../Components/SelectThirdPartyAccountModal";
 
 function Room() {
     const { roomId } = useParams();
+    const [showModal, setShowModal] = React.useState(false);
+
     return (
         <>
-            <div className="flex flex-col sm:flex-row items-center justify-around w-full h-48 py-5 shadow-lg bg-dark-eye" >
+            <div className="flex flex-col md:flex-row items-center justify-around w-full h-48 py-5 shadow-lg bg-dark-eye" >
                 <div className='flex w-full justify-center'>
-                    <div className="hidden sm:w-1/4 sm:block"/>
-                    <div className='sm:w-3/4 relative font-bold text-2xl m-5 px-3 sm:px-0 text-gray-200'>
+                    <div className="hidden md:w-1/4 sm:block"/>
+                    <div className='md:w-3/4 relative font-bold text-lg md:text-2xl m-5 px-3 sm:px-0 text-gray-200'>
                         { roomId }
                     </div>
                 </div>
 
-                <div className='flex w-full items-center justify-center sm:justify-end sm:mr-4'>
+                <div className='flex w-full items-center justify-center md:justify-end md:mr-4'>
                     <Link to={`/room/${roomId}/external-url`} >
                         <button className="block m-3 px-6 py-3 bg-green-600 hover:bg-indigo-600 focus:outline-none rounded">
                             <div className="text-sm font-medium leading-none text-white">
@@ -22,9 +25,13 @@ function Room() {
                         </button>
                     </Link>
 
-                    <button className="block m-3 px-6 py-3 bg-indigo-700 hover:bg-indigo-600 focus:outline-none rounded">
+                    <button
+                    className="block m-3 px-6 py-3 bg-indigo-700 hover:bg-indigo-600 focus:outline-none rounded"
+                    onClick={() => setShowModal(true)}
+                    >
                         <p className="text-sm font-medium leading-none text-white">Add Condition</p>
                     </button>
+                    <SelectThirdPartyAccountModal showModal={showModal} setShowModal={() => setShowModal()}/>
                 </div>
 
                 {/* <div className='hidden sm:w-2/3 sm:block'/> */}
