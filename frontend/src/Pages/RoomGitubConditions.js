45750ba2cf86e34794f3db0d74c517b8de9f31b2
kuries <bineshmunukurthi@gmail.com>
2022-07-26 18:20:24 +0530

    Design UI for dropdowns in github conditions page with static data.

--- frontend/src/Pages/RoomGitubConditions.js
+++ frontend/src/Pages/RoomGitubConditions.js
@@ -5,6 +5,42 @@ import { Link, useParams } from 'react-router-dom'
 export default function RoomGitubConditions() {
   const { roomId } = useParams();
 
+  const buttonUserUI = (
+      <button className="flex justify-start items-center w-full h-10 px-5 text-black hover:bg-gray-400">
+        <div className='flex justify-start items-center w-1/2'>
+          <div className='px-4 py-1 mr-2 bg-gray-900 text-white rounded-lg font-medium'>
+            User
+          </div>
+
+          <div className='mx-2 font-medium'>
+            Kuries
+          </div>
+        </div>
+
+        <div className='flex justify-end w-1/2'>
+          <svg className="w-4 h-4 ml-3 fill-current" viewBox="0 0 20 20"><path d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" fillRule="evenodd"></path></svg>
+        </div>
+      </button>
+  );
+
+  const buttonOrgUI = (
+    <button className="flex justify-start items-center w-full h-10 px-5 text-black">
+      <div className='flex justify-start items-center w-1/2'>
+        <div className='px-4 py-1 mr-2 bg-gray-900 text-white rounded-lg font-medium'>
+          Org
+        </div>
+
+        <div className='mx-2 font-medium'>
+          TestOrgForCerberus
+        </div>
+      </div>
+
+      <div className='flex justify-end w-1/2'>
+        <svg className="w-4 h-4 ml-3 fill-current" viewBox="0 0 20 20"><path d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" fillRule="evenodd"></path></svg>
+      </div>
+    </button>
+);
+
   return (
     <>
       <Helmet>
@@ -28,6 +64,22 @@ export default function RoomGitubConditions() {
                   </button>
               </Link>
           </div>
+      </div>
+
+      <div className="w-full px-6 lg:mx-auto lg:w-10/12">
+        <div className="w-full my-3 px-4 pt-4 pb-5 overflow-y-auto">
+          <div className='my-4 p-2 bg-gray-200 border border-dark-eye rounded-md hover:bg-gray-400'>
+            {buttonUserUI}
+          </div>
+
+          <div className='my-4 p-2 bg-gray-200 border border-dark-eye rounded-md hover:bg-gray-400'>
+            {buttonOrgUI}
+          </div>
+
+          <div className='my-4 p-2 bg-gray-200 border border-dark-eye rounded-md hover:bg-gray-400'>
+            {buttonOrgUI}
+          </div>
+        </div>
       </div>
     </>
   )
