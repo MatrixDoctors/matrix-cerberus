87f856be65dd0c66021283d9468f714aa77e99e6
kuries <bineshmunukurthi@gmail.com>
2022-07-26 21:31:37 +0530

    Add nested dropdowns and buttons for different github condition types under a user/org.

--- frontend/src/Pages/RoomGitubConditions.js
+++ frontend/src/Pages/RoomGitubConditions.js
@@ -1,37 +1,35 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 import { Link, useParams } from 'react-router-dom'
+import PropTypes from "prop-types"
 
-export default function RoomGitubConditions() {
-  const { roomId } = useParams();
-
-  const buttonUserUI = (
-      <button className="flex justify-start items-center w-full h-10 px-5 text-black hover:bg-gray-400">
-        <div className='flex justify-start items-center w-1/2'>
-          <div className='px-4 py-1 mr-2 bg-gray-900 text-white rounded-lg font-medium'>
-            User
-          </div>
-
-          <div className='mx-2 font-medium'>
-            Kuries
-          </div>
+function Table() {
+  return (
+    <div className='w-full my-2 p-2 bg-gray-300'>
+      <div className='flex-col items-center bg-white rounded-md'>
+        <div className='w-full p-3 rounded-md border-b border-gray-200 bg-white hover:bg-gray-100'>
+          SampleRepo 1
         </div>
 
-        <div className='flex justify-end w-1/2'>
-          <svg className="w-4 h-4 ml-3 fill-current" viewBox="0 0 20 20"><path d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" fillRule="evenodd"></path></svg>
+        <div className='w-full p-3 rounded-md border-b border-gray-200 bg-white hover:bg-gray-100'>
+          SampleRepo 2
         </div>
-      </button>
-  );
+      </div>
+    </div>
+  )
+}
 
-  const buttonOrgUI = (
-    <button className="flex justify-start items-center w-full h-10 px-5 text-black">
+function ConditionsDropdown({isUser, isOpen}) {
+
+  const buttonUI = (
+    <button className="flex justify-start items-center w-full h-10 border-b border-dark-eye text-black">
       <div className='flex justify-start items-center w-1/2'>
         <div className='px-4 py-1 mr-2 bg-gray-900 text-white rounded-lg font-medium'>
-          Org
+          {isUser ? 'User': 'Org'}
         </div>
 
         <div className='mx-2 font-medium'>
-          TestOrgForCerberus
+          {isUser ? 'Kuries': 'TestOrgForCerberus'}
         </div>
       </div>
 
@@ -39,7 +37,56 @@ export default function RoomGitubConditions() {
         <svg className="w-4 h-4 ml-3 fill-current" viewBox="0 0 20 20"><path d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" fillRule="evenodd"></path></svg>
       </div>
     </button>
-);
+  );
+
+  function InsideButton({type, isTableOpen}) {
+    return (
+      <div className='w-full my-4 p-2 rounded-md bg-gray-300'>
+        <button className="flex justify-start items-center w-full h-10 text-black">
+          <div className='flex justify-start w-1/3'>
+            <div className='px-4 py-1 mr-2 rounded-lg font-medium'>
+              {type}
+            </div>
+          </div>
+
+          <div className='flex justify-end w-2/3'>
+            <svg className="w-4 h-4 ml-3 fill-current" viewBox="0 0 20 20"><path d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" fillRule="evenodd"></path></svg>
+          </div>
+        </button>
+
+        {isTableOpen ? <Table /> : <></>}
+
+      </div>
+    )
+  }
+
+  InsideButton.propTypes = {
+    type: PropTypes.string,
+    isTableOpen: PropTypes.bool
+  }
+
+  const internalUI = (
+    <div className='flex-col items-center w-full bg-white'>
+      <InsideButton type={'Repositories'} isTableOpen={true}/>
+      <InsideButton type={'Teams'} isTableOpen={false}/>
+    </div>
+  )
+
+  return (
+    <div className='my-4 p-2 rounded-md'>
+      {buttonUI}
+      {isOpen ? internalUI : <></>}
+    </div>
+  )
+}
+
+ConditionsDropdown.propTypes = {
+  isUser: PropTypes.bool,
+  isOpen: PropTypes.bool
+}
+
+export default function RoomGitubConditions() {
+  const { roomId } = useParams();
 
   return (
     <>
@@ -68,17 +115,9 @@ export default function RoomGitubConditions() {
 
       <div className="w-full px-6 lg:mx-auto lg:w-10/12">
         <div className="w-full my-3 px-4 pt-4 pb-5 overflow-y-auto">
-          <div className='my-4 p-2 bg-gray-200 border border-dark-eye rounded-md hover:bg-gray-400'>
-            {buttonUserUI}
-          </div>
-
-          <div className='my-4 p-2 bg-gray-200 border border-dark-eye rounded-md hover:bg-gray-400'>
-            {buttonOrgUI}
-          </div>
-
-          <div className='my-4 p-2 bg-gray-200 border border-dark-eye rounded-md hover:bg-gray-400'>
-            {buttonOrgUI}
-          </div>
+          <ConditionsDropdown isUser={true} isOpen={true}/>
+          <ConditionsDropdown isUser={false} isOpen={false}/>
+          <ConditionsDropdown isUser={false} isOpen={false}/>
         </div>
       </div>
     </>
